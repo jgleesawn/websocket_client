693a2d9206d9af2ba90ab8e6f30e68ec87d0213a
jgleesawn <j.gleesawn@gmail.com>
2014-06-21 23:20:27 -0800

    First upload to github.

--- client.go
+++ client.go
@@ -12,18 +12,22 @@ func main() {
 	var port string
 	var url string
 	if len(os.Args) < 3 {
-		port = "5000"
+		port = "80"
 	} else {
 		port = os.Args[2]
 	}
 	if len(os.Args) < 2 {
-		url = "localhost"
+		url = "onelyfe.herokuapp.com"
 	} else {
 		url = os.Args[1]
 	}
 
 //Look into WebSocket Keys, not sure using the same one every time is good.
 	resp, err := http.Get("http://"+url+":"+port)
+	for i := range resp.Header {
+		fmt.Println(i+":"+resp.Header[i][0])
+	}
+	fmt.Println()
 	if err != nil {
 		panic(err)
 	}
@@ -33,12 +37,20 @@ func main() {
 	resp.Header.Add("Sec-WebSocket-Key","x3JJHMbDL1EzLkh9GBhXDw==")
 	resp.Header.Add("Sec-WebSocket-Protocol","chat, superchat")
 	resp.Header.Add("Sec-WebScoket-Version","13")
-	resp.Header.Add("Origin","http://"+url+":"+port)
+	resp.Header.Add("Origin","http://"+url+":"+port+"/ws")
+	for i := range resp.Header {
+		fmt.Println(i+":"+resp.Header[i][0])
+	}
+	fmt.Println()
 
 
 //middle argument is response header.
 	var DefaultDialer *websocket.Dialer
-	conn, _,err := DefaultDialer.Dial("ws://"+url+":" + port + "/ws",resp.Header)
+	conn, resp,err := DefaultDialer.Dial("ws://"+url+":" + port + "/ws",resp.Header)
+	for i := range resp.Header {
+		fmt.Println(i+":"+resp.Header[i][0])
+	}
+	fmt.Println()
 	if err != nil {
 		panic(err)
 	}
